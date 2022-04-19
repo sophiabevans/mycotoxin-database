1b6a44a7610ea0a243434222e6d42743b2c89f52
sophiabevans <sophiajbevans@gmail.com>
2022-04-19 15:48:13 -0400

    added scripts and csv files to repository

---
+++ scripts/database_clean.R
@@ -0,0 +1,71 @@
+library(tidyverse)
+library(readxl)
+
+mycotoxin_df <- read_excel("~/BostonUniversity/BF768/homework/database_project/BiologicalRemovalOfMycotoxins.xlsx")
+
+col_info <- mycotoxin_df[1,]
+
+mycotoxin_df <- mycotoxin_df %>% 
+  filter(!is.na(`#`)) %>%
+  select(-`#`)
+
+#make domains consistent
+dom <- str_replace_all(str_to_title(mycotoxin_df$Domain), pattern = "Eukary.*", replacement = "Eukarya")
+
+#pathogen categories: Pathogenic, Non-Pathogenic, Opportunistic 
+paths <- str_to_title(mycotoxin_df$`Pathogen?`)
+paths <- str_replace_all(string = paths, pattern = ".*Opport.*", replacement = "Opportunistic")
+paths <- str_replace_all(string = paths, pattern = ".*Non.Path.*", replacement = "Non-Pathogenic")
+paths <- str_replace_all(string = paths, pattern = ".* Path.*|Path.*", replacement = "Pathogenic")
+
+#aerobic or anaerobic: make consistent (all values either "Aerobic", "Anaerobic", or "Facultative")
+aran <- str_to_title(mycotoxin_df$`Aerobic or Anaerobic?`)
+aran<- str_replace_all(string = aran, pattern = ".*Facult.*", replacement = "Facultative")
+aran<- str_replace_all(string = aran, pattern = ".*Anaer.*", replacement = "Anaerobic")
+aran<- str_replace_all(string = aran, pattern = ".*Aer.*", replacement = "Aerobic")
+
+#native environment: lots of info in this one so add column with all values in 
+#("Human", "Soil", "Water", "Plants", "Other")
+
+#removal mechanism in ("Biotransformation", "Absorption", "Adsorption", "Degradation", "Regulation", Unknown")
+
+#enzymatic
+enz <- str_to_title(mycotoxin_df$`Enzymatic?`)
+enz <- str_replace(string = enz, pattern = "Unk.*", replacement = "Unknown")
+
+#intra/extra cellular
+cell <- str_to_title(mycotoxin_df$`Intracellular or Extracellular?`)
+cell <- str_replace(string = cell, pattern = "Un.*|Likely.*|Na", replacement = "Unknown")
+cell <- str_replace(string = cell, pattern = "Extra.*", replacement = "Extracellular")
+cell <- str_replace(string = cell, pattern = "Intra.*", replacement = "Intracellular")
+
+mycotoxin_df <- mycotoxin_df %>%
+  mutate(Domain = dom, 
+         `Pathogenicity` = paths, 
+         `Respiration` = aran, 
+         `Mycotoxin` = str_to_title(`Target mycotoxin`),
+         `Enzymatic?` = enz,
+         `Location` = cell %>% replace_na("Unknown"),
+         `Enzyme identified?`= str_to_title(`Enzyme identified?`)) %>%
+  rename("Environment" = "Native environment")
+  
+
+write_csv(mycotoxin_df, file = "~/BostonUniversity/BF768/homework/database_project/mycotoxin_removal.csv")
+
+org <- mycotoxin_df %>%
+  select(Organism, Domain, Pathogenicity, Respiration, Environment)
+write_csv(org, file = "~/BostonUniversity/BF768/homework/database_project/organism.csv")
+
+lit <- mycotoxin_df %>%
+  select(`Characterization context`, `Characterization assay`, Source, Link, `Additional information`)
+write_csv(lit, file = "~/BostonUniversity/BF768/homework/database_project/literature.csv")
+  
+cur_cont <- mycotoxin_df %>%
+  select(Contributor, `Contribution date`, `Curator(s)`, `Curation date`, `Curation notes`)
+write_csv(cur_cont, file = "~/BostonUniversity/BF768/homework/database_project/curation.csv")
+
+mycotoxin <- mycotoxin_df %>%
+  select(`Mycotoxin`, `Removal mechanism`, `Enzymatic?`, Location)
+write_csv(mycotoxin, file = "~/BostonUniversity/BF768/homework/database_project/mycotoxin.csv")
+
+
---
+++ scripts/database_execute.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+import pymysql
+import sys
+
+# to print program usage when called with no parameters
+# note that sys.arv[0] is the program name
+if len(sys.argv) != 3: #number of parameters +1 for program name
+   print ("Usage is: %s username password" % sys.argv[0])
+   sys.exit(0)
+
+usr = sys.argv[1]
+pswd  = sys.argv[2]
+
+# connecting Python to MySQL
+connection = pymysql.connect(
+host="bioed.bu.edu",
+db="Group_M",
+user=usr,
+passwd=pswd,
+port=4253,
+local_infile=True)
+
+#connection.begin()
+cursor = connection.cursor()
+
+queries = ['DROP TABLE IF EXISTS Removal, Curation_Contribution, Organism, Mycotoxin, Literature;',
+"""
+CREATE TABLE Literature (
+LID INTEGER NOT NULL AUTO_INCREMENT,
+Context VARCHAR(100),
+Assay VARCHAR(100),
+Source VARCHAR(100),
+Link VARCHAR(100),
+PRIMARY KEY(LID)
+);
+""",
+"""
+CREATE TABLE Organism (
+OID INTEGER NOT NULL AUTO_INCREMENT,
+Domain VARCHAR(20),
+Name VARCHAR(30),
+Pathogenicity VARCHAR(30),
+Respiration VARCHAR(30),
+Environment VARCHAR(30),
+PRIMARY KEY(OID)
+);
+""",
+"""
+CREATE TABLE Mycotoxin (
+MID INTEGER NOT NULL AUTO_INCREMENT,
+Removal_mech VARCHAR(30),
+Enzymatic_or_not VARCHAR(20),
+Location VARCHAR(30),
+PRIMARY KEY(MID)
+);
+""",
+"""
+CREATE TABLE Curation_Contribution (
+CID INTEGER NOT NULL AUTO_INCREMENT,
+Con_name VARCHAR(30),
+Con_date DATE,
+Cur_name VARCHAR(30),
+Cur_date DATE,
+Cur_notes VARCHAR(200),
+PRIMARY KEY(CID)
+);
+""",
+"""
+CREATE TABLE Removal (
+OID INT NOT NULL,
+MID INT NOT NULL,
+LID INT NOT NULL,
+PRIMARY KEY(OID,MID),
+FOREIGN KEY(OID) REFERENCES Organism(OID),
+FOREIGN KEY(MID) REFERENCES Mycotoxin(MID),
+FOREIGN KEY(LID) REFERENCES Literature(LID)
+);
+""",
+'load data local infile "curation.csv" into table Curation_Contribution fields terminated by "," (Con_name, Con_date, Cur_name, Cur_date, Cur_notes);',
+'load data local infile "mycotoxin.csv" into table Mycotoxin fields terminated by "," (Removal_mech, Enzymatic_or_not, Location);',
+'load data local infile "organism.csv" into table Organism fields terminated by "," (Domain, Name, Pathogenicity, Respiration, Environment);',
+'load data local infile "literature.csv" into table Literature fields terminated by "," (Context, Assay, Source, Link);']
+
+for q in queries:
+	try:
+	    cursor.execute(q)
+	except pymysql.Error as e:
+	    print(e)
+
+#commit changes
+connection.commit()
+
+#close cursor and connection
+cursor.close()
+connection.close()
