7db8a6afbb97ba3c166c9cd7b41b83eb40584580
sophiabevans <sophiajbevans@gmail.com>
2022-04-20 10:22:09 -0400

    build dictionary through loop instead of comprehension for duplicate keys

--- scripts/database_execute.py
+++ scripts/database_execute.py
@@ -4,27 +4,27 @@ import sys
 
 # to print program usage when called with no parameters
 # note that sys.arv[0] is the program name
-if len(sys.argv) != 3: #number of parameters +1 for program name
-   print ("Usage is: %s username password" % sys.argv[0])
+if len(sys.argv) != 3:  # number of parameters +1 for program name
+   print("Usage is: %s username password" % sys.argv[0])
    sys.exit(0)
 
 usr = sys.argv[1]
-pswd  = sys.argv[2]
+pswd = sys.argv[2]
 
 # connecting Python to MySQL
 connection = pymysql.connect(
-host="bioed.bu.edu",
-db="Group_M",
-user=usr,
-passwd=pswd,
-port=4253,
-local_infile=True)
+    host="bioed.bu.edu",
+    db="Group_M",
+    user=usr,
+    passwd=pswd,
+    port=4253,
+    local_infile=True)
 
 #connection.begin()
 cursor = connection.cursor()
 
 queries = ['DROP TABLE IF EXISTS Removal, Curation_Contribution, Organism, Mycotoxin, Literature;',
-"""
+           """
 CREATE TABLE Literature (
 LID INTEGER NOT NULL AUTO_INCREMENT,
 Context VARCHAR(100),
@@ -34,7 +34,7 @@ Link VARCHAR(100),
 PRIMARY KEY(LID)
 );
 """,
-"""
+           """
 CREATE TABLE Organism (
 OID INTEGER NOT NULL AUTO_INCREMENT,
 Domain VARCHAR(40),
@@ -45,7 +45,7 @@ Environment VARCHAR(100),
 PRIMARY KEY(OID)
 );
 """,
-"""
+           """
 CREATE TABLE Mycotoxin (
 MID INTEGER NOT NULL AUTO_INCREMENT,
 Name VARCHAR(100),
@@ -55,7 +55,7 @@ Location VARCHAR(50),
 PRIMARY KEY(MID)
 );
 """,
-"""
+           """
 CREATE TABLE Curation_Contribution (
 CID INTEGER NOT NULL AUTO_INCREMENT,
 Con_name VARCHAR(30),
@@ -66,7 +66,7 @@ Cur_notes VARCHAR(200),
 PRIMARY KEY(CID)
 );
 """,
-"""
+           """
 CREATE TABLE Removal (
 OID INT NOT NULL,
 MID INT NOT NULL,
@@ -77,10 +77,10 @@ FOREIGN KEY(MID) REFERENCES Mycotoxin(MID),
 FOREIGN KEY(LID) REFERENCES Literature(LID)
 );
 """,
-'load data local infile "../data/curation.tsv" into table Curation_Contribution ignore 1 lines (Con_name, Con_date, Cur_name, Cur_date, Cur_notes);',
-'load data local infile "../data/mycotoxin.tsv" into table Mycotoxin ignore 1 lines (Name, Removal_mech, Enzymatic_or_not, Location);',
-'load data local infile "../data/organism.tsv" into table Organism ignore 1 lines (Name, Domain, Pathogenicity, Respiration, Environment);',
-'load data local infile "../data/literature.tsv" into table Literature ignore 1 lines (Context, Assay, Source, Link);']
+           'load data local infile "../data/curation.tsv" into table Curation_Contribution ignore 1 lines (Con_name, Con_date, Cur_name, Cur_date, Cur_notes);',
+           'load data local infile "../data/mycotoxin.tsv" into table Mycotoxin ignore 1 lines (Name, Removal_mech, Enzymatic_or_not, Location);',
+           'load data local infile "../data/organism.tsv" into table Organism ignore 1 lines (Name, Domain, Pathogenicity, Respiration, Environment);',
+           'load data local infile "../data/literature.tsv" into table Literature ignore 1 lines (Context, Assay, Source, Link);']
 
 for q in queries:
 	try:
@@ -101,18 +101,35 @@ lids = cursor.fetchall()
 cursor.execute("select Link from Literature;")
 lnames = cursor.fetchall()
 
-organism_dict = {i[0]:j[0] for i,j in zip(onames, oids)}
-#TODO: fix multiple ids to be in list
-mycotoxin_dict = {i[0]:j[0] for i,j in zip(mnames, mids)}
-lit_dict = {i[0]:j[0] for i,j in zip(lnames, lids)}
+organism_dict = {}
+mycotoxin_dict = {}
+lit_dict = {}
+
+for i, j in zip(onames, oids):
+    if i in organism_dict:
+        organism_dict[i] += j
+    else:
+        organism_dict[i] = [j]
+
+for i, j in zip(mnames, mids):
+    if i in mycotoxin_dict:
+        mycotoxin_dict[i] += j
+    else:
+        mycotoxin_dict[i] = [j]
+
+for i, j in zip(lnames, lids):
+    if i in lit_dict:
+        lit_dict[i] += j
+    else:
+        lit_dict[i] = [j]
 
 with open("../data/mycotoxin_removal.tsv", "r") as f:
-    line = f.readline()
+    line = f.readline()  # skip header
     line = f.readline()
     while line:
         fields = line.strip().split("\t")
         lid = lit_dict[fields[1]]
-        oid= organism_dict[fields[2]]
+        oid = organism_dict[fields[2]]
         mid = mycotoxin_dict[fields[7]]
         cursor.execute(f"""insert into Removal (OID, MID, LID)
         values ({oid}, {mid}, {lid})""")
