cb18cf8c5babab47a69850c8ab384373ea64c0fd
mranawee <98359567+mranawee@users.noreply.github.com>
2022-04-21 16:39:54 -0400

    Rough cgi draft

---
+++ scripts/ProjectCGI.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+import cgi
+import pymysql
+#from getpass import getpass
+#pswd = getpass()
+
+#the next two lines are useful for debugging
+#they cause errors during execution to be sent back to the browser
+import cgitb
+cgitb.enable()
+
+#the next line gives us a convenient way to insert values into strings
+from string import Template 
+
+
+html_form = """
+            
+            """
+#retrieve form data from the web server
+form = cgi.FieldStorage() 
+
+
+#next line is always required as first part of html output
+print("Content-type: text/html\n")
+
+if (form):
+    connection = pymysql.connect(host='bioed.bu.edu', 
+                                 port=4253, 
+                                 user='mranawee', 
+                                 database='miRNA', 
+                                 password = 'Megaspyguy4')
+    cursor = connection.cursor()
+    query = """
+            """
+            
+    try:
+        cursor.execute(query)
+        
+    except pymysql.error as e:
+        
+else:
+    html_table = """
+    
+                 """
