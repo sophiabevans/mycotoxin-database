e294fb5cc5b81b36125dd5d302869b0b30ea0e44
sophiabevans <sophiajbevans@gmail.com>
2022-05-03 00:16:03 -0400

    spacing issue

--- scripts/database_execute.py
+++ scripts/database_execute.py
@@ -157,7 +157,7 @@ with open("../data/mycotoxinN.csv", "r") as f:
 
         if N in litN:
             try:
-            	cursor.execute(f"""
+                cursor.execute(f"""
                 insert into Literature (Context, Assay, Source, Link)
                 values ({Char_con}, {Char_assay}, {Source}, {Link})""")
                 cursor.execute("set @lid = LAST_INSERT_ID();")
@@ -165,7 +165,7 @@ with open("../data/mycotoxinN.csv", "r") as f:
                 print(e)
         if N in mycN:
             try:
-            	cursor.execute(f"""
+                cursor.execute(f"""
                 insert into Mycotoxin (Name, Removal_mech, Enzymatic_or_not, Location)
                 values ({Mycotoxin}, {Removal_mech}, {Enzymatic}, {Location})""")
                 cursor.execute("set @mid = LAST_INSERT_ID();")
@@ -173,7 +173,7 @@ with open("../data/mycotoxinN.csv", "r") as f:
                 print(e)
         if N in orgN:
             try:
-            	cursor.execute(f"""
+                cursor.execute(f"""
                 insert into Organism (Domain, Name, Pathogenicity, Respiration, Environment)
                 values ({Domain}, {Organism}, {Pathogenicity}, {Respiration}, {Environment})""")
                 cursor.execute("set @oid = LAST_INSERT_ID();")
@@ -181,7 +181,7 @@ with open("../data/mycotoxinN.csv", "r") as f:
                 print(e)
         if N in curconN:
             try:
-            	cursor.execute(f"""
+                cursor.execute(f"""
                 insert into Curation_Contribution (Con_name, Con_date, Cur_name, Cur_date, Cur_notes)
                 values ({Contributor}, {Cont_date}, {Curator}, {Cur_date}, {Cur_notes})""")
                 cursor.execute("set @cid = LAST_INSERT_ID();")
@@ -191,6 +191,8 @@ with open("../data/mycotoxinN.csv", "r") as f:
             cursor.execute(f"""
             insert into Removal (OID, MID, LID, CID)
             values (@oid @mid @lid @cid)""")
+        except pymysql.Error as e:
+            print(e)
        	line = f.readline()
 
 #commit changes
