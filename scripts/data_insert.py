74ba386fc4900c8ab8e0c39e4ab7f6d41f235246
sophiabevans <sophiajbevans@gmail.com>
2022-05-06 17:13:11 -0400

    debugging - remove fields

--- scripts/data_insert.py
+++ scripts/data_insert.py
@@ -37,6 +37,7 @@ if (form):
       input_domain = form.getvalue("input_domain", "")
       input_path = form.getvalue("input_path", "")
       input_aeran = form.getvalue("input_aeran", "")
+      #input_env = form.getlist("input_env", "")
       # env_hum = form.getvalue("env_hum", "")
       # env_anim = form.getvalue("env_anim", "")
       # env_plant = form.getvalue("env_plant", "")
@@ -44,10 +45,10 @@ if (form):
       # env_water = form.getvalue("env_water", "")
       # env_food = form.getvalue("env_food", "")
       # env_other = form.getvalue("env_other", "")
-      # myc_name= form.getvalue("myc_name", "")
-      # removal = form.getvalue("removal", "")
-      # enzymatic = form.getvalue("enzymatic", "")
-      # loc = form.getvalue("loc", "")
+      myc_name= form.getvalue("myc_name", "")
+      removal = form.getvalue("removal", "")
+      enzymatic = form.getvalue("enzymatic", "")
+      loc = form.getvalue("loc", "")
       #
       # env = ""
       # for e in [env_anim, env_food, env_hum, env_other, env_plant, env_soil, env_water]:
