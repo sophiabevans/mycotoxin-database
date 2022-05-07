3513fcb8231c12be71bc188fce5e660dbb73a969
sophiabevans <sophiajbevans@gmail.com>
2022-05-06 23:19:35 -0400

    debug

--- scripts/data_insert_test.py
+++ scripts/data_insert_test.py
@@ -37,44 +37,44 @@ if (form):
     input_domain = form.getvalue("input_domain", "")
     input_path = form.getvalue("input_path", "")
     input_aeran = form.getvalue("input_aeran", "")
-    env_hum = form.getvalue("env_hum", "")
-    env_anim = form.getvalue("env_anim", "")
-    env_plant = form.getvalue("env_plant", "")
-    env_soil = form.getvalue("env_soil", "")
-    env_water = form.getvalue("env_water", "")
-    env_food = form.getvalue("env_food", "")
-    env_other = form.getvalue("env_other", "")
-    if env_other != "":
-        env_other = form.getvalue("other_env_txt", "").title()
-    rem_biotran = form.getvalue("biotran", "")
-    rem_reg = form.getvalue("reg", "")
-    rem_adsorp = form.getvalue("adsorp", "")
-    rem_absorp = form.getvalue("absorp", "")
-    rem_deg = form.getvalue("deg", "")
-    rem_reg = form.getvalue("reg", "")
-    rem_unknown = form.getvalue("unknown_rem", "")
-    rem_other = form.getvalue("other_rem", "")
-    if rem_other != "":
-        rem_other = form.getvalue("other_input_rem", "").title()
-    myc_name = form.getvalue("myc_name", "").title()
-    enzymatic = form.getvalue("enzymatic", "")
-    loc = form.getvalue("loc", "")
-
-    env = ""
-    envs = [env_anim, env_food, env_hum,
-            env_other, env_plant, env_soil, env_water]
-    envs.sort()
-    for e in envs:
-        if e != "":
-            env += f"{e};"
-
-    rem = ""
-    rems = [rem_biotran, rem_reg, rem_adsorp, rem_absorp,
-            rem_deg, rem_reg, rem_unknown, rem_other]
-    rems.sort()
-    for r in rems:
-        if r != "":
-            rem += f"{r};"
+    # env_hum = form.getvalue("env_hum", "")
+    # env_anim = form.getvalue("env_anim", "")
+    # env_plant = form.getvalue("env_plant", "")
+    # env_soil = form.getvalue("env_soil", "")
+    # env_water = form.getvalue("env_water", "")
+    # env_food = form.getvalue("env_food", "")
+    # env_other = form.getvalue("env_other", "")
+    # if env_other != "":
+    #     env_other = form.getvalue("other_env_txt", "").title()
+    # rem_biotran = form.getvalue("biotran", "")
+    # rem_reg = form.getvalue("reg", "")
+    # rem_adsorp = form.getvalue("adsorp", "")
+    # rem_absorp = form.getvalue("absorp", "")
+    # rem_deg = form.getvalue("deg", "")
+    # rem_reg = form.getvalue("reg", "")
+    # rem_unknown = form.getvalue("unknown_rem", "")
+    # rem_other = form.getvalue("other_rem", "")
+    # if rem_other != "":
+    #     rem_other = form.getvalue("other_input_rem", "").title()
+    # myc_name = form.getvalue("myc_name", "").title()
+    # enzymatic = form.getvalue("enzymatic", "")
+    # loc = form.getvalue("loc", "")
+    #
+    # env = ""
+    # envs = [env_anim, env_food, env_hum,
+    #         env_other, env_plant, env_soil, env_water]
+    # envs.sort()
+    # for e in envs:
+    #     if e != "":
+    #         env += f"{e};"
+    #
+    # rem = ""
+    # rems = [rem_biotran, rem_reg, rem_adsorp, rem_absorp,
+    #         rem_deg, rem_reg, rem_unknown, rem_other]
+    # rems.sort()
+    # for r in rems:
+    #     if r != "":
+    #         rem += f"{r};"
 
     try:
         cursor.execute(f'''
@@ -84,22 +84,22 @@ if (form):
     except pymysql.Error as e:
         print(e)
         success = False
-    try:
-        cursor.execute(f'''
-        insert into Mycotoxin (Name, Removal_mech, Enzymatic_or_not, Location)
-        values ("{myc_name}", "{rem}", "{enzymatic}", "{loc}");''')
-        cursor.execute("set @mid = LAST_INSERT_ID();")
-    except pymysql.Error as e:
-        print(e)
-        success = False
-    try:
-        cursor.execute(f'''
-        insert into Organism (Domain, Name, Pathogenicity, Respiration, Environment)
-        values ("{input_domain}", "{org_name}", "{input_path}", "{input_aeran}", "{env}");''')
-        cursor.execute("set @oid = LAST_INSERT_ID();")
-    except pymysql.Error as e:
-        print(e)
-        success = False
+    # try:
+    #     cursor.execute(f'''
+    #     insert into Mycotoxin (Name, Removal_mech, Enzymatic_or_not, Location)
+    #     values ("{myc_name}", "{rem}", "{enzymatic}", "{loc}");''')
+    #     cursor.execute("set @mid = LAST_INSERT_ID();")
+    # except pymysql.Error as e:
+    #     print(e)
+    #     success = False
+    # try:
+    #     cursor.execute(f'''
+    #     insert into Organism (Domain, Name, Pathogenicity, Respiration, Environment)
+    #     values ("{input_domain}", "{org_name}", "{input_path}", "{input_aeran}", "{env}");''')
+    #     cursor.execute("set @oid = LAST_INSERT_ID();")
+    # except pymysql.Error as e:
+    #     print(e)
+    #     success = False
     try:
         cursor.execute(f'''
         insert into Curation_Contribution (Con_name, Con_date, Cur_name, Cur_date, Cur_notes, Additional_info)
@@ -108,13 +108,13 @@ if (form):
     except pymysql.Error as e:
         print(e)
         success = False
-    try:
-        cursor.execute('''
-        insert into Removal (OID, MID, LID, CID)
-        values (@oid, @mid, @lid, @cid);''')
-    except pymysql.Error as e:
-        print(e)
-        success = False
+    # try:
+    #     cursor.execute('''
+    #     insert into Removal (OID, MID, LID, CID)
+    #     values (@oid, @mid, @lid, @cid);''')
+    # except pymysql.Error as e:
+    #     print(e)
+    #     success = False
 else:
     success = False
 
