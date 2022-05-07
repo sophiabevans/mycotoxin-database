a64b0f543b11693de8ae377b803fba761080cf4a
sophiabevans <sophiajbevans@gmail.com>
2022-05-07 01:49:42 -0400

    test by committing changes

--- scripts/data_insert_test.py
+++ scripts/data_insert_test.py
@@ -92,14 +92,19 @@ if (form):
         success = False
 
     try:
-        cursor.execute(
-            "UPDATE table Organism SET value=NULL WHERE value='NULL';")
-        cursor.execute(
-            "UPDATE table Mycotoxin SET value=NULL WHERE value='NULL';")
-        cursor.execute(
-            "UPDATE table Curation_Contribution SET value=NULL WHERE value='NULL';")
-        cursor.execute(
-            "UPDATE table Literature SET value=NULL WHERE value='NULL';")
+        for value in ["Domain", "Pathogenicity", "Respiration", "Enviroment"]:
+            cursor.execute(
+                f"UPDATE table Organism SET {value}=NULL WHERE {value}='NULL' and OID=@oid;")
+        for value in ["Removal_mech", "Enzymatic_or_not", "Location"]:
+            cursor.execute(
+                f"UPDATE table Mycotoxin SET {value}=NULL WHERE {value}='NULL' and MID=@mid;")
+        for value in ["Con_name", "Cur_notes", "Additional_info"]:
+            cursor.execute(
+                f"UPDATE table Curation_Contribution SET {value}=NULL WHERE {value}='NULL'; and CID=@cid")
+        for value in ["Context", "Assay", "Link"]:
+            cursor.execute(
+                f"UPDATE table Literature SET {value}=NULL WHERE {value}='NULL' and LID=@lid;")
+
 else:
     success = False
 
