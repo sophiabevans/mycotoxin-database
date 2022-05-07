8b6ecaa018f63e2e947346f9dc0a174dc89c3c1f
sophiabevans <sophiajbevans@gmail.com>
2022-05-06 21:11:56 -0400

    debug

--- scripts/data_insert_test.py
+++ scripts/data_insert_test.py
@@ -20,44 +20,43 @@ except pymysql.Error as e:
     success = False
 cursor = connection.cursor()
 
-# next line is always required as first part of http output
 print("Content-type: text/html\n")
-#
-# if (form):
-#     link = form.getvalue("link", "")
-#     cite = form.getvalue("cite", "")
-#     context = form.getvalue("context", "")
-#     assay = form.getvalue("assay", "")
-#     cur_name = form.getvalue("curator", "")
-#     cur_notes = form.getvalue("cur_notes", "")
-#     cur_date = form.getvalue("cur_date", "")
-#     con_name = form.getvalue("con_name", "")
-#     con_date = form.getvalue("con_date", "")
-#     ad_info = form.getvalue("ad_info", "")
-#     org_name = form.getvalue("org_name", "")
-#     input_domain = form.getvalue("input_domain", "")
-#     input_path = form.getvalue("input_path", "")
-#     input_aeran = form.getvalue("input_aeran", "")
-#     # input_env = form.getlist("input_env", "")
-#     env_hum = form.getvalue("env_hum", "")
-#     env_anim = form.getvalue("env_anim", "")
-#     env_plant = form.getvalue("env_plant", "")
-#     env_soil = form.getvalue("env_soil", "")
-#     env_water = form.getvalue("env_water", "")
-#     env_food = form.getvalue("env_food", "")
-#     env_other = form.getvalue("env_other", "")
-#     myc_name = form.getvalue("myc_name", "")
-#     removal = form.getvalue("removal", "")
-#     enzymatic = form.getvalue("enzymatic", "")
-#     loc = form.getvalue("loc", "")
-#
-#     env = ""
-#     envs = [env_anim, env_food, env_hum,
-#             env_other, env_plant, env_soil, env_water]
-#     for e in envs.sort():
-#         if e != "":
-#             env += f"{e};"
-#
+
+if (form):
+    link = form.getvalue("link", "")
+    cite = form.getvalue("cite", "")
+    context = form.getvalue("context", "")
+    assay = form.getvalue("assay", "")
+    cur_name = form.getvalue("curator", "")
+    cur_notes = form.getvalue("cur_notes", "")
+    cur_date = form.getvalue("cur_date", "")
+    con_name = form.getvalue("con_name", "")
+    con_date = form.getvalue("con_date", "")
+    ad_info = form.getvalue("ad_info", "")
+    org_name = form.getvalue("org_name", "")
+    input_domain = form.getvalue("input_domain", "")
+    input_path = form.getvalue("input_path", "")
+    input_aeran = form.getvalue("input_aeran", "")
+    # input_env = form.getlist("input_env", "")
+    env_hum = form.getvalue("env_hum", "")
+    env_anim = form.getvalue("env_anim", "")
+    env_plant = form.getvalue("env_plant", "")
+    env_soil = form.getvalue("env_soil", "")
+    env_water = form.getvalue("env_water", "")
+    env_food = form.getvalue("env_food", "")
+    env_other = form.getvalue("env_other", "")
+    myc_name = form.getvalue("myc_name", "")
+    removal = form.getvalue("removal", "")
+    enzymatic = form.getvalue("enzymatic", "")
+    loc = form.getvalue("loc", "")
+
+    env = ""
+    envs = [env_anim, env_food, env_hum,
+            env_other, env_plant, env_soil, env_water]
+    for e in envs.sort():
+        if e != "":
+            env += f"{e};"
+
 #     try:
 #         cursor.execute(f'''
 #         insert into Literature (Context, Assay, Source, Link)
@@ -97,8 +96,8 @@ print("Content-type: text/html\n")
 #     except pymysql.Error as e:
 #         print(e)
 #         success = False
-# else:
-#     success = False
+else:
+    success = False
 
 if success:
     results = "Your data insertion was successful!"
