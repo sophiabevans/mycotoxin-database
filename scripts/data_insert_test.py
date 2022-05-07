86f8a7f0f704187bab30eb42761328ba49bb8ba6
sophiabevans <sophiajbevans@gmail.com>
2022-05-07 02:02:41 -0400

    test by committing changes - update nulls

--- scripts/data_insert_test.py
+++ scripts/data_insert_test.py
@@ -91,21 +91,21 @@ if (form):
         print(e)
         success = False
 
-    try:
-        for value in ["Domain", "Pathogenicity", "Respiration", "Enviroment"]:
-            cursor.execute(
-                f"UPDATE Organism SET {value}=NULL WHERE {value} regexp 'NULL' and OID=@oid;")
-        for value in ["Removal_mech", "Enzymatic_or_not", "Location"]:
-            cursor.execute(
-                f"UPDATE Mycotoxin SET {value}=NULL WHERE {value} regexp 'NULL' and MID=@mid;")
-        for value in ["Con_name", "Cur_notes", "Additional_info"]:
-            cursor.execute(
-                f"UPDATE Curation_Contribution SET {value}=NULL WHERE {value} regexp 'NULL' and CID=@cid;")
-        for value in ["Context", "Assay", "Link"]:
-            cursor.execute(
-                f"UPDATE Literature SET {value}=NULL WHERE {value} regexp 'NULL' and LID=@lid;")
-    except pymysql.Error as e:
-        print(e)
+    # try:
+    #     for value in ["Domain", "Pathogenicity", "Respiration", "Enviroment"]:
+    #         cursor.execute(
+    #             f"UPDATE Organism SET {value}=NULL WHERE {value}='NULL' and OID=@oid;")
+    #     for value in ["Removal_mech", "Enzymatic_or_not", "Location"]:
+    #         cursor.execute(
+    #             f"UPDATE Mycotoxin SET {value}=NULL WHERE {value}='NULL' and MID=@mid;")
+    #     for value in ["Con_name", "Cur_notes", "Additional_info"]:
+    #         cursor.execute(
+    #             f"UPDATE Curation_Contribution SET {value}=NULL WHERE {value}='NULL' and CID=@cid;")
+    #     for value in ["Context", "Assay", "Link"]:
+    #         cursor.execute(
+    #             f"UPDATE Literature SET {value}=NULL WHERE {value}='NULL' and LID=@lid;")
+    # except pymysql.Error as e:
+    #     print(e)
 
 else:
     success = False
